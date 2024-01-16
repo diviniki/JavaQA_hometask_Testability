da4c2e0861ea524717088caa955e904d90af773e
dmitriyn <d.nikitin.rf@gmail.com>
2024-01-16 22:36:33 +0500

    add new scripts

---
+++ src/JavaTask3_1.java
@@ -0,0 +1,11 @@
+public class JavaTask3_1 {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.price(price); // должно получиться 500
+        System.out.println(miles);
+
+        //int myBonus = service.price(10000);
+        //System.out.println(myBonus);
+    }
+}
